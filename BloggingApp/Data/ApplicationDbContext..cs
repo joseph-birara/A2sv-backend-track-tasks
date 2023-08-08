45da57c8c25117903fc1a65522b6aafd0eb50741
joseph-birara <josephbirara92@gamil.com>
2023-08-08 14:23:58 +0300

    bloging app starter file added

---
+++ BloggingApp/Data/ApplicationDbContext..cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using BloggingApp.Models;
+
+namespace BloggingApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure entity relationships and constraints here if needed.
+        }
+    }
+}
---
+++ BloggingApp/Models/Post.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BloggingApp.WebApi.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
