3d4bd1a3bc4827a18fe08e23646cba377843a0e0
joseph-birara <josephbirara92@gmail.com>
2023-08-16 00:57:11 +0300

    clean architecture web api

--- CleanBlogApi/BlogApi.Application/Interfaces/IPostService.cs
+++ CleanBlogApi/BlogApi.Application/Interfaces/IPostService.cs
@@ -1,17 +1,4 @@
-// IPostService.cs
-using BlogApi.Domain.Entities;
-using System.Collections.Generic;
 
-namespace BlogApi.Application.Interfaces
-{
-    public interface IPostService
-    {
-        Post CreatePost(string title, string content);
-        Post GetPostById(int postId);
-        List<Post> GetPosts();
-        // Other methods for updating, deleting, etc.
-    }
-}
 
 using BlogApi.Domain.Entities;
 using System.Collections.Generic;
